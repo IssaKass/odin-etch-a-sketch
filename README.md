c500143d3186ecda511c5775987805c7e62b6c7d
Abdallah-Issa Kass <issakass02@gmail.com>
2025-02-20 17:16:12 +0200

    Create Etch-A-Sketch Project
    - Defined the whole HTML structure and the CSS styles.
    - Included header and footer.
    - Add a toolbar and a grid.
    - Defined the whole JS logic.
    
    -- Features
    - Light/Dark Theme:
    - Color Palette
    - Dynamic Grid
    - Toggle Borders
    - Brush, rainbow and eraser modes
    - Clear grid
    - One active mode at a time

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # odin-etch-a-sketch
+
+Welcome to Etch-a-Sketch, a simple Pixel Sketch creator. This project is built using HTML, CSS and JavaScript as part of [The Odin Project](https://www.theodinproject.com/dashboard) curriculum.
+
+## 🚀 Live Preview
+
+You can view the live preview of the application by clicking the link below:
+
+🔗 [Live Preview](https://issakass.github.io/odin-etch-a-sketch/)
+
+## Features
+
+### 🎨 Color Customization
+
+- Light/Dark Theme: Switch between light and dark themes for a comfortable sketching experience.
+- Color Palette: Choose colors using hex codes or RGBA values for precise color control.
+
+### 📐 Dynamic Canvas
+
+- Dynamic Grid: Adjust the grid size to create detailed or larger pixel art.
+- Toggle Borders: Show or hide grid borders for a clean or structured look.
+
+### ✏️ Drawing Modes
+
+- Brush Mode: Draw with the selected color from the palette.
+- Rainbow Mode: Paint with random colors for a fun, vibrant effect.
+- Eraser Mode: Erase any colored pixel and revert it to the background color.
+
+### 🛠️ Additional Features
+
+- Clear Grid: Instantly clear the canvas to start a new drawing.
+- One Active Mode at a Time: Only one mode button stays active, preventing mode conflicts and enhancing usability.
---
+++ main.js
@@ -0,0 +1,127 @@
+// DOM Elements
+const changeThemeButton = document.querySelector("#change-theme");
+
+const grid = document.querySelector("#grid");
+const gridColorInput = document.querySelector("#grid-color-input");
+const gridSizeMessage = document.querySelector("#grid-size-message");
+const gridSizeInput = document.querySelector("#grid-size-input");
+const clearButton = document.querySelector("#clear-button");
+const toggleBordersButton = document.querySelector("#toggle-borders-button");
+const toolbar = document.querySelector(".toolbar");
+
+// States
+let gridSize = 16;
+let gridColor = "black";
+let currentMode = "brush";
+let isMouseDown = false;
+let showBorders = true;
+
+// Initialize Grid
+document.addEventListener("DOMContentLoaded", initializeGrid);
+
+// Event Listeners
+changeThemeButton.addEventListener("click", changeTheme);
+
+gridColorInput.addEventListener("input", () => updateMode("brush"));
+gridSizeInput.addEventListener("input", updateGrid);
+clearButton.addEventListener("click", clearGrid);
+toggleBordersButton.addEventListener("click", () =>
+	updateBorders(!showBorders)
+);
+
+grid.addEventListener("mousedown", (event) => {
+	isMouseDown = true;
+	handleDrawing(event);
+});
+grid.addEventListener("mouseup", () => (isMouseDown = false));
+grid.addEventListener("mouseover", handleDrawing);
+
+toolbar.addEventListener("click", function (event) {
+	const button = event.target.closest("button");
+
+	if (!button.classList.contains("mode-button")) return;
+
+	updateMode(button.dataset.mode);
+});
+
+// Functions
+function initializeGrid() {
+	updateGrid();
+	updateBorders(true);
+	updateMode("brush");
+}
+
+function changeTheme() {
+	const currentTheme = document.body.dataset.theme;
+
+	if (currentTheme === "light") {
+		changeThemeButton.querySelector("i").classList.replace("fa-sun", "fa-moon");
+		document.body.dataset.theme = "dark";
+	} else {
+		changeThemeButton.querySelector("i").classList.replace("fa-moon", "fa-sun");
+		document.body.dataset.theme = "light";
+	}
+}
+
+function updateGrid() {
+	gridSize = gridSizeInput.value;
+	gridSizeMessage.textContent = `Grid size: ${gridSize} x ${gridSize}`;
+
+	grid.innerHTML = "";
+	grid.style.setProperty("--grid", gridSize);
+
+	for (let i = 0; i < gridSize ** 2; i++) {
+		const gridCell = document.createElement("div");
+		gridCell.classList.add("grid-cell");
+		gridCell.style.setProperty("--color", "");
+		grid.append(gridCell);
+	}
+
+	updateMode("brush");
+}
+
+function clearGrid() {
+	updateGrid();
+	updateMode("brush");
+}
+
+function updateBorders(state) {
+	showBorders = state;
+	toggleBordersButton.classList.toggle("active", showBorders);
+	grid.classList.toggle("show-borders", showBorders);
+}
+
+function updateMode(mode) {
+	currentMode = mode;
+	toolbar
+		.querySelectorAll(".mode-button")
+		.forEach((button) =>
+			button.classList.toggle("active", currentMode === button.dataset.mode)
+		);
+}
+
+function handleDrawing(event) {
+	if (!event.target.classList.contains("grid-cell") || !isMouseDown) return;
+
+	if (currentMode === "brush") {
+		gridColor = gridColorInput.value;
+		event.target.style.setProperty("--color", gridColor);
+	} else if (currentMode === "rainbow") {
+		gridColor = generateRandomColor();
+		gridColorInput.value = gridColor;
+		event.target.style.setProperty("--color", gridColor);
+	} else if (currentMode === "eraser") {
+		event.target.style.setProperty("--color", "");
+	}
+}
+
+function generateRandomColor() {
+	const letters = "0123456789ABCDEF";
+
+	let color = "#";
+	for (let i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+
+	return color;
+}
